4d9804550b56b6dfe5f7e7709e19d8f34c998b13
Abdelrhmn <abdlrhmntt@gmail.com>
2024-06-09 22:01:36 +0300

    .

---
+++ site.js
@@ -0,0 +1,11 @@
+// caeousel section 
+const carouselContainer = document.querySelector('.carouselContainer');
+const carouselItems = document.querySelectorAll('.carouselItem');
+let currentIndex = 0;
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+setInterval(showNextImage, 3000); // Change image every 3 seconds
