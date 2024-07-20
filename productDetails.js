ddacac121a8a72643fd4977fe83f73929f457e34
Abdelrhmn <abdlrhmntt@gmail.com>
2024-07-20 03:35:37 +0300

    ..

--- productDetails.js
+++ productDetails.js
@@ -90,8 +90,59 @@ function displayProduct(product) {
         // For example, assuming there's a container with class 'product-info'
         document.querySelector('.product-info').appendChild(quantityDiv);
            
-        // Additional properties to be displayed can be added here
-        displayProducts(getRelatedProducts(productId));
+       // RELATED PRODUCTS 
+       // Display related products
+       if (product.relatedproducts && product.relatedproducts.length > 0) {
+        product.relatedproducts.forEach(relatedId => {
+            const relatedProduct = products.find(p => p.id === relatedId);
+            if (relatedProduct) {
+                const relatedProductDiv = document.createElement('div');
+                relatedProductDiv.classList.add('related-product-item');
+                relatedProductDiv.innerHTML = `
+                <div class="card">
+                <div class="child-card">
+                    <a href="productDetails.html?id=${relatedProduct.Id}" class="img-link">
+                        <img src="images/${relatedProduct.ImagePath1}" alt="${relatedProduct.Name}">
+                    </a>
+                    <a href="">
+                        <h3>${relatedProduct.Name}</h3>
+                    </a>
+                    <p class="price">${relatedProduct.Price} ج.م</p>
+                    <p class="end-card">
+                        <button onclick="addToCart(${relatedProduct.Id})">
+                            <svg class="cart-left" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" viewBox="0 0 16 16">
+                                <path
+                                    d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0" />
+                            </svg>
+                            <span>أضف الي السلة</span>
+                        </button>
+                        <!--Heart-->
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="heart">
+                            <path
+                                d="M12 5.50063L11.4596 6.02073C11.463 6.02421 11.4664 6.02765 11.4698 6.03106L12 5.50063ZM8.96173 18.9109L8.49742 19.4999L8.96173 18.9109ZM15.0383 18.9109L14.574 18.3219L15.0383 18.9109ZM7.00061 16.4209C6.68078 16.1577 6.20813 16.2036 5.94491 16.5234C5.68169 16.8432 5.72758 17.3159 6.04741 17.5791L7.00061 16.4209ZM2.34199 13.4115C2.54074 13.7749 2.99647 13.9084 3.35988 13.7096C3.7233 13.5108 3.85677 13.0551 3.65801 12.6917L2.34199 13.4115ZM13.4698 8.03034C13.7627 8.32318 14.2376 8.32309 14.5304 8.03014C14.8233 7.7372 14.8232 7.26232 14.5302 6.96948L13.4698 8.03034ZM2.75 9.1371C2.75 6.98623 3.96537 5.18252 5.62436 4.42419C7.23607 3.68748 9.40166 3.88258 11.4596 6.02073L12.5404 4.98053C10.0985 2.44352 7.26409 2.02539 5.00076 3.05996C2.78471 4.07292 1.25 6.42503 1.25 9.1371H2.75ZM8.49742 19.4999C9.00965 19.9037 9.55955 20.3343 10.1168 20.6599C10.6739 20.9854 11.3096 21.25 12 21.25V19.75C11.6904 19.75 11.3261 19.6293 10.8736 19.3648C10.4213 19.1005 9.95208 18.7366 9.42605 18.3219L8.49742 19.4999ZM15.5026 19.4999C16.9292 18.3752 18.7528 17.0866 20.1833 15.4758C21.6395 13.8361 22.75 11.8026 22.75 9.1371H21.25C21.25 11.3345 20.3508 13.0282 19.0617 14.4798C17.7469 15.9603 16.0896 17.1271 14.574 18.3219L15.5026 19.4999ZM22.75 9.1371C22.75 6.42503 21.2153 4.07292 18.9992 3.05996C16.7359 2.02539 13.9015 2.44352 11.4596 4.98053L12.5404 6.02073C14.5983 3.88258 16.7639 3.68748 18.3756 4.42419C20.0346 5.18252 21.25 6.98623 21.25 9.1371H22.75ZM14.574 18.3219C14.0479 18.7366 13.5787 19.1005 13.1264 19.3648C12.6739 19.6293 12.3096 19.75 12 19.75V21.25C12.6904 21.25 13.3261 20.9854 13.8832 20.6599C14.4405 20.3343 14.9903 19.9037 15.5026 19.4999L14.574 18.3219ZM9.42605 18.3219C8.63014 17.6945 7.82129 17.0963 7.00061 16.4209L6.04741 17.5791C6.87768 18.2624 7.75472 18.9144 8.49742 19.4999L9.42605 18.3219ZM3.65801 12.6917C3.0968 11.6656 2.75 10.5033 2.75 9.1371H1.25C1.25 10.7746 1.66995 12.1827 2.34199 13.4115L3.65801 12.6917ZM11.4698 6.03106L13.4698 8.03034L14.5302 6.96948L12.5302 4.97021L11.4698 6.03106Z"
+                                fill="#56b0f2" />
+                        </svg>
+    
+                        <!--Quick View-->
+                        <svg fill="#000000" viewBox="-3.5 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            class="show-svg">
+                            <title>view</title>
+                            <path
+                                d="M12.406 13.844c1.188 0 2.156 0.969 2.156 2.156s-0.969 2.125-2.156 2.125-2.125-0.938-2.125-2.125 0.938-2.156 2.125-2.156zM12.406 8.531c7.063 0 12.156 6.625 12.156 6.625 0.344 0.438 0.344 1.219 0 1.656 0 0-5.094 6.625-12.156 6.625s-12.156-6.625-12.156-6.625c-0.344-0.438-0.344-1.219 0-1.656 0 0 5.094-6.625 12.156-6.625zM12.406 21.344c2.938 0 5.344-2.406 5.344-5.344s-2.406-5.344-5.344-5.344-5.344 2.406-5.344 5.344 2.406 5.344 5.344 5.344z">
+                            </path>
+                        </svg>
+                    </p>
+                </div>
+            </div>
+                `;
+                relatedProductsContainer.appendChild(relatedProductDiv);
+            }
+        });
+    } else {
+        relatedProductsContainer.innerHTML = '<p>No related products found.</p>';
+    }
+       
     } else {
         document.querySelector('.product-container').textContent = 'Product not found';
     }
@@ -138,34 +189,34 @@ function displayDescriptions(descriptions) {
 
 
 //GET RELATED PRODUCTS 
-export async function getRelatedProducts(productid) {
-    try {
-      // Fetch the products from the JSON file
-      const response = await fetch('products.json');
-      const products = await response.json();
+// export async function getRelatedProducts(productid) {
+//     try {
+//       // Fetch the products from the JSON file
+//       const response = await fetch('products.json');
+//       const products = await response.json();
   
-      // Find the product with the given ID
-      const product = products.find(p => p.id === productid);
+//       // Find the product with the given ID
+//       const product = products.find(p => p.id === productid);
   
-      if (!product) {
-        throw new Error('Product not found');
-      }
+//       if (!product) {
+//         throw new Error('Product not found');
+//       }
   
-      // Filter the products to get the related ones
-      const relatedProducts = products.filter(p => product.relatedProducts.includes(p.id));
+//       // Filter the products to get the related ones
+//       const relatedProducts = products.filter(p => product.relatedProducts.includes(p.id));
   
-      return relatedProducts;
-    } catch (error) {
-      console.error('Error fetching related products:', error);
-      return [];
-    }
-}
+//       return relatedProducts;
+//     } catch (error) {
+//       console.error('Error fetching related products:', error);
+//       return [];
+//     }
+// }
 
-import {createProductCard} from './site'
+// import {createProductCard} from './site'
 
-//displayProducts(getRelatedProducts(productId));
+// //displayProducts(getRelatedProducts(productId));
 
-function displayProducts(products) {
-    const productsArea = document.querySelector('.products-area-from-details-page');
-    productsArea.innerHTML = products.map(product => createProductCard(product)).join('');
-}
+// function displayProducts(products) {
+//     const productsArea = document.querySelector('.products-area-from-details-page');
+//     productsArea.innerHTML = products.map(product => createProductCard(product)).join('');
+// }
